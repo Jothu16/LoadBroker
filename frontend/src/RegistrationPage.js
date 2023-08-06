6d5017210f6d872f58d97bcb3a1e676b09694f5d
Harjot Grewal <hsgrewal16@gmail.com>
2023-08-05 23:56:33 -0700

    Fixed Registration Page, now ready to do DB

--- frontend/src/RegistrationPage.js
+++ frontend/src/RegistrationPage.js
@@ -1,17 +1,46 @@
 import React from 'react';
 import './RegisterPage.css';
+import { useState } from 'react';
 
 
 // This component provides a registration form for new users.
 function RegistrationPage() {
+
+    const handleRegistration = (event) => {
+    event.preventDefault(); // Prevent default form submission behavior
+
+    // Here, you'll send the form data to the backend
+    console.log("User Registration Data:", { username, password, email });
+    };
+
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [email, setEmail] = useState('');
+
     return (
         <div className="registration-page">
             <h2>Register</h2>
-            <form>
+            <form onSubmit={handleRegistration}> {/* Add the onSubmit attribute here */}
                 {/* Input fields for username, password, and email */}
-                <input type="text" placeholder="Username" />
-                <input type="password" placeholder="Password" />
-                <input type="email" placeholder="Email" />
+                <input 
+                    type="text" 
+                    placeholder="Username" 
+                    value={username} 
+                    onChange={(e) => setUsername(e.target.value)}
+                />
+                <input 
+                    type="password" 
+                    placeholder="Password" 
+                    value={password} 
+                    onChange={(e) => setPassword(e.target.value)}
+                />
+                <input 
+                    type="email" 
+                    placeholder="Email" 
+                    value={email} 
+                    onChange={(e) => setEmail(e.target.value)}
+                />
+
                 {/* Registration button */}
                 <button type="submit">Register</button>
             </form>
@@ -19,4 +48,6 @@ function RegistrationPage() {
     );
 }
 
+
 export default RegistrationPage;
+
