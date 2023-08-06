8845e334ef9611629f13973f65a09fa2753c12ad
Harjot Grewal <hsgrewal16@gmail.com>
2023-08-05 21:50:25 -0700

    Added react and pages

---
+++ frontend/src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+// This component provides a login form for existing users.
+function LoginPage() {
+    return ( <
+        div className = "login-page" >
+        <
+        h2 > Login < /h2> <
+        form > { /* Input fields for username and password */ } <
+        input type = "text"
+        placeholder = "Username" / >
+        <
+        input type = "password"
+        placeholder = "Password" / > { /* Login button */ } <
+        button type = "submit" > Login < /button> < /
+        form > <
+        /div>
+    );
+}
+
+export default LoginPage;
