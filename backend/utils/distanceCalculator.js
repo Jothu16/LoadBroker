a7fbe45c84e6843830541fcded6cfd4891b2242c
Harjot Grewal <hsgrewal16@gmail.com>
2023-08-13 12:44:35 -0700

    Separated utils.js, deleted it as well

---
+++ backend/utils/distanceCalculator.js
@@ -0,0 +1,8 @@
+// Function to calculate distance between origin and destination
+function getDistance(origin, destination) {
+    // You can implement the logic to calculate the distance here
+    // For now, I'll return a placeholder value
+    return 1000; // Placeholder value in miles or kilometers
+}
+
+export default getDistance;
